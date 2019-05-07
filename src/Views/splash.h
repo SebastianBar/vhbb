ab9ff293545b4a5d33fd6e536257a76523dca742
Sebastián Barrenechea <sebastian@dealsyte.com>
2019-05-06 20:26:28 -0400

    Faster splashscreen for faster app loading time

--- src/Views/splash.h
+++ src/Views/splash.h
@@ -6,11 +6,7 @@
 #include <texture.h>
 
 #define SPLASH_FADING_STEP_SIZE 4
-#ifdef DEBUG
-	#define SPLASH_STATIC_DURATION_IN_FRAMES 2*60
-#else
-	#define SPLASH_STATIC_DURATION_IN_FRAMES 5*60
-#endif
+#define SPLASH_STATIC_DURATION_IN_FRAMES 2*60
 
 typedef enum {
 	STEP_FADING_IN,
